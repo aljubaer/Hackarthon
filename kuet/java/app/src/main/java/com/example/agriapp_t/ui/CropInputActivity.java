0cb3881e6c1e34a4a974dafb62d21712952dd5d6
khayrulislam <bsse0822@iit.du.ac.bd>
2019-09-05 12:53:54 +0600

    new activity created for cropInput Activity

---
+++ kuet/java/app/src/main/java/com/example/agriapp_t/ui/CropInputActivity.java
@@ -0,0 +1,16 @@
+package com.example.agriapp_t.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.agriapp_t.R;
+
+public class CropInputActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crop_input);
+    }
+}
