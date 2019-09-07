ddaea788c01fd9a4c93c7e938de6a906c3ea0898
khayrulislam <bsse0822@iit.du.ac.bd>
2019-09-07 09:55:30 +0600

    nutrient input ui and funconality added

---
+++ kuet/java/app/src/main/java/com/example/agriapp_t/ui/nutrient_input/NutrientInputActivity.java
@@ -0,0 +1,45 @@
+package com.example.agriapp_t.ui.nutrient_input;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import com.example.agriapp_t.R;
+
+public class NutrientInputActivity extends AppCompatActivity {
+
+    TextView NitrogenAmount;
+    double nitrogenAmount;
+    String nitrogenAmountText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nutrient_input);
+        NitrogenAmount = findViewById(R.id.nitrogen_amount);
+    }
+
+    public void onRadioButtonClickedNitrogen(View view){
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio_low_nitrogen:
+                if (checked)
+                    nitrogenAmountText = "low";
+                    break;
+            case R.id.radio_medium_nitrogen:
+                if (checked)
+                    nitrogenAmountText = "medium";
+                    break;
+            case R.id.radio_high_nitrogen:
+                if (checked)
+                    nitrogenAmountText = "medium";
+                break;
+        }
+        NitrogenAmount.setText("low");
+
+    }
+}
